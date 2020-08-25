00a0a99d10bccd6ec98eb8fb545f51cbcf6f209f
sustainablelab <sustainablelab@gmail.com>
2020-08-24 23:50:29 -0400

    Create a main() entry point in __main__.py, move GUI docstring to __init__.py

---
+++ microspecgui/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""Chromation dev-kit interactive application.
+
+Install
+-------
+$ pip install microspecgui
+
+Run
+---
+$ microspec-gui
+
+Keyboard controls
+-----------------
+q   - quit
+a   - auto-expose (spacebar works also)
+x   - decrease exposure time
+X   - increase exposure time
+h/l - navigate wavelength slow
+j/k - navigate wavelength fast
+0   - go to shortest wavelength
+$   - go to longest wavelength
+
+Joystick controls
+-----------------
+BACK - quit
+A    - auto-expose
+X    - decrease exposure
+Y    - increase exposure
+right-stick   - navigate wavelength slow
+left-stick    - navigate wavelength fast
+left-trigger  - go to shortest wavelength
+right-trigger - go to longest wavelength
+"""
+
